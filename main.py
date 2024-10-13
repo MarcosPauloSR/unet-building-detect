5ea01974e47147f2a07108bb05370007fc90bb68
Marcos Paulo <marcospaulosrezende@gmail.com>
2024-10-13 19:32:15 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,247 @@
+import os
+from PIL import Image
+import numpy as np
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+image_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+mask_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor()
+])
+
+
+class SegmentationDataset(Dataset):
+    def __init__(self, images_dir, masks_dir, image_transform=None, mask_transform=None):
+        self.images_dir = images_dir
+        self.masks_dir = masks_dir
+        self.image_transform = image_transform
+        self.mask_transform = mask_transform
+        self.image_names = os.listdir(images_dir)
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, idx):
+        image_name = self.image_names[idx]
+        image_path = os.path.join(self.images_dir, image_name)
+        mask_path = os.path.join(self.masks_dir, image_name)
+
+        image = Image.open(image_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")
+
+        if self.image_transform:
+            image = self.image_transform(image)
+        if self.mask_transform:
+            mask = self.mask_transform(mask)
+            mask = (mask > 0).float()  # Binarização
+
+        return image, mask
+
+dataset = SegmentationDataset(images_dir='train/images/',
+                              masks_dir='train/masks/',
+                              image_transform=image_transform,
+                              mask_transform=mask_transform)
+
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+
+import torch
+import torch.nn as nn
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1):
+        super(UNet, self).__init__()
+
+        # Encoder
+        self.enc1 = self.double_conv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.enc2 = self.double_conv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.enc3 = self.double_conv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.enc4 = self.double_conv(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        # Bottleneck
+        self.bottom = self.double_conv(512, 1024)
+
+        # Decoder
+        self.upconv4 = self.up_conv(1024, 512)
+        self.dec4 = self.double_conv(1024, 512)
+
+        self.upconv3 = self.up_conv(512, 256)
+        self.dec3 = self.double_conv(512, 256)
+
+        self.upconv2 = self.up_conv(256, 128)
+        self.dec2 = self.double_conv(256, 128)
+
+        self.upconv1 = self.up_conv(128, 64)
+        self.dec1 = self.double_conv(128, 64)
+
+        # Camada final
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def double_conv(self, in_channels, out_channels):
+        """(conv => BN => ReLU) * 2"""
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),  
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),  
+            nn.ReLU(inplace=True),
+        )
+
+    def up_conv(self, in_channels, out_channels):
+        """Upscaling"""
+        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+    def forward(self, x):
+        # Encoder
+        x1 = self.enc1(x)      # [N, 64, H, W]
+        x1p = self.pool1(x1)   # [N, 64, H/2, W/2]
+
+        x2 = self.enc2(x1p)    # [N, 128, H/2, W/2]
+        x2p = self.pool2(x2)   # [N, 128, H/4, W/4]
+
+        x3 = self.enc3(x2p)    # [N, 256, H/4, W/4]
+        x3p = self.pool3(x3)   # [N, 256, H/8, W/8]
+
+        x4 = self.enc4(x3p)    # [N, 512, H/8, W/8]
+        x4p = self.pool4(x4)   # [N, 512, H/16, W/16]
+
+        # Bottleneck
+        x5 = self.bottom(x4p)  # [N, 1024, H/16, W/16]
+
+        # Decoder
+        x6 = self.upconv4(x5)  # [N, 512, H/8, W/8]
+        x6 = torch.cat([x6, x4], dim=1)  # [N, 1024, H/8, W/8]
+        x6 = self.dec4(x6)     # [N, 512, H/8, W/8]
+
+        x7 = self.upconv3(x6)  # [N, 256, H/4, W/4]
+        x7 = torch.cat([x7, x3], dim=1)  # [N, 512, H/4, W/4]
+        x7 = self.dec3(x7)     # [N, 256, H/4, W/4]
+
+        x8 = self.upconv2(x7)  # [N, 128, H/2, W/2]
+        x8 = torch.cat([x8, x2], dim=1)  # [N, 256, H/2, W/2]
+        x8 = self.dec2(x8)     # [N, 128, H/2, W/2]
+
+        x9 = self.upconv1(x8)  # [N, 64, H, W]
+        x9 = torch.cat([x9, x1], dim=1)  # [N, 128, H, W]
+        x9 = self.dec1(x9)     # [N, 64, H, W]
+
+        # Saída
+        output = self.final_conv(x9)  # [N, out_channels, H, W]
+        return output
+    
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = UNet(in_channels=3, out_channels=1)
+model = model.to(device)
+
+criterion = nn.BCEWithLogitsLoss()
+
+images, masks = next(iter(dataloader))
+images = images.to(device)
+masks = masks.to(device)
+
+with torch.no_grad():
+    outputs = model(images)
+    probs = torch.sigmoid(outputs)
+    preds = (probs > 0.5).float()
+
+
+# Instanciação do modelo
+model = model.to(device)
+
+# Otimizador e função de perda
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+criterion = nn.BCEWithLogitsLoss()
+
+num_epochs = 25
+
+# Loop de treinamento simplificado
+for epoch in range(num_epochs):
+    model.train()
+    for images, masks in dataloader:
+        images = images.to(device)
+        masks = masks.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, masks)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(f'Época [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+import matplotlib.pyplot as plt
+
+model.eval()
+with torch.no_grad():
+    images, masks = next(iter(dataloader))
+    images = images.to(device)
+    outputs = model(images)
+    probs = torch.sigmoid(outputs)
+    preds = (probs > 0.5).float()
+
+torch.save(model.state_dict(), 'unet_model.pth')
+
+# Converter tensores para CPU e numpy
+# images = images.cpu().numpy()
+# masks = masks.cpu().numpy()
+# preds = preds.cpu().numpy()
+
+# Função de denormalização
+def denormalize(image_tensor):
+    mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(image_tensor.device)
+    std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(image_tensor.device)
+    image_tensor = image_tensor * std + mean
+    return image_tensor
+
+
+# Denormalizar imagens enquanto ainda são tensores PyTorch
+images_denorm = denormalize(images)
+
+# Converter tensores para CPU e então para NumPy
+images_np = images_denorm.permute(0, 2, 3, 1).cpu().numpy()
+masks_np = masks.cpu().numpy()
+preds_np = preds.cpu().numpy()
+
+# Plotar as imagens, máscaras verdadeiras e predições
+for i in range(len(images_np)):
+    plt.figure(figsize=(12, 4))
+    
+    # Imagem Original
+    plt.subplot(1, 3, 1)
+    plt.imshow(np.clip(images_np[i], 0, 1))
+    plt.title('Imagem Original')
+    plt.axis('off')
+    
+    # Máscara Verdadeira
+    plt.subplot(1, 3, 2)
+    plt.imshow(masks_np[i][0], cmap='gray')
+    plt.title('Máscara Verdadeira')
+    plt.axis('off')
+    
+    # Predição da Máscara
+    plt.subplot(1, 3, 3)
+    plt.imshow(preds_np[i][0], cmap='gray')
+    plt.title('Predição da Máscara')
+    plt.axis('off')
+    
+    plt.show()
+
---
+++ test.py
@@ -0,0 +1,107 @@
+from torchvision import transforms
+from PIL import Image
+from main import UNet
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# Definição das transformações
+image_transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+def load_image(image_path):
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f"O arquivo {image_path} não foi encontrado.")
+    image = Image.open(image_path).convert("RGB")
+    image = image_transform(image)
+    image = image.unsqueeze(0)  # Adiciona a dimensão do batch
+    return image
+
+# Definição do dispositivo
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print('Está sendo utilizado o dispositivo:', device)
+
+# Instanciação do modelo
+model = UNet(in_channels=3, out_channels=1)
+model = model.to(device)
+
+# Carregamento dos pesos
+model.load_state_dict(torch.load('unet_model.pth', map_location=device))
+
+# Definir o modelo em modo de avaliação
+model.eval()
+
+def predict_image(image_path):
+    # Carrega e pré-processa a imagem
+    image = load_image(image_path)
+    image = image.to(device)
+    
+    # Desabilita o cálculo de gradientes
+    with torch.no_grad():
+        output = model(image)
+        output = torch.sigmoid(output)
+        # Binarização da saída
+        predicted_mask = (output > 0.5).float()
+    
+    return predicted_mask.cpu().squeeze(0)  # Remove a dimensão do batch
+
+def denormalize(image_tensor):
+    mean = torch.tensor([0.485, 0.456, 0.406]).view(3,1,1)
+    std = torch.tensor([0.229, 0.224, 0.225]).view(3,1,1)
+    image = image_tensor.cpu().clone()
+    image = image * std + mean
+    image = image.clamp(0, 1)
+    return image
+
+def visualize_prediction(image_path):
+    # Carrega e pré-processa a imagem
+    image = load_image(image_path)
+    image_denorm = denormalize(image.squeeze(0))
+    image_np = image_denorm.permute(1, 2, 0).numpy()
+    
+    # Obtém a predição
+    predicted_mask = predict_image(image_path)
+    mask_np = predicted_mask.squeeze().numpy()
+    
+    # Plotagem
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.imshow(image_np)
+    plt.title('Imagem Original')
+    plt.axis('off')
+    
+    plt.subplot(1, 2, 2)
+    plt.imshow(mask_np, cmap='gray')
+    plt.title('Máscara Predita')
+    plt.axis('off')
+    
+    plt.show()
+
+def save_predicted_mask(image_path, save_path):
+    predicted_mask = predict_image(image_path)
+    # Converte o tensor para PIL Image
+    mask_image = transforms.ToPILImage()(predicted_mask)
+    mask_image.save(save_path)
+
+# Diretório das imagens
+image_directory = './val/images/'
+
+# Listar as imagens disponíveis
+available_images = [f for f in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, f))]
+print("Imagens disponíveis na pasta './val/images/':")
+for img in available_images:
+    print(img)
+
+# Verificar se 'ifgoiano.png' está na lista
+if 'ifgoiano.png' in available_images:
+    image_path = os.path.join(image_directory, 'ifgoiano.png')
+    visualize_prediction(image_path)
+    save_predicted_mask(image_path, 'mascara_predita.png')
+else:
+    print("A imagem 'ifgoiano.png' não foi encontrada na pasta './val/images/'. Por favor, verifique o nome do arquivo e sua extensão.")
